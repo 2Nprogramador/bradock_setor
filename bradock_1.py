14800f14600846225876d7e99e324e48f2566220
2Nprogramador <nunolfreitas@gmail.com>
2024-11-25 18:11:31 -0300

    Update bradock_1.py

--- bradock_1.py
+++ bradock_1.py
@@ -7,6 +7,7 @@ import os
 from google.oauth2.service_account import Credentials
 import json
 import toml
+from datetime import datetime, timedelta
 
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
